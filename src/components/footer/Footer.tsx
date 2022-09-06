f284224638793ebf6429147673c2a0a6ff1ea445
河野太平 <>
2022-09-06 18:34:25 +0900

    material-uiとfontawesomeを導入

--- src/components/footer/Footer.tsx
+++ src/components/footer/Footer.tsx
@@ -1,6 +1,27 @@
+import { BottomNavigation, BottomNavigationAction, Paper } from "@mui/material";
+import { useState } from "react";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faShare } from '@fortawesome/free-solid-svg-icons';
+
 function Footer () {
+    const [value, setValue] = useState(0);
+
     return (
-        <footer>これがフッター</footer>
+        <footer>
+            <Paper sx={{ position: 'fixed', bottom: 0, left: 0, right: 0, paddingBottom: '0.7rem' }} elevation={3}>
+                <BottomNavigation
+                    showLabels
+                    value={value}
+                    onChange={(event, newValue) => {
+                      setValue(newValue);
+                    }}
+                >
+                    <BottomNavigationAction label="何か" icon={<FontAwesomeIcon icon={faShare} />} />
+                    <BottomNavigationAction label="何か" icon={<FontAwesomeIcon icon={faShare} />} />
+                    <BottomNavigationAction label="何か" icon={<FontAwesomeIcon icon={faShare} />} />
+                </BottomNavigation>
+            </Paper>
+        </footer>
     )
 }
 
