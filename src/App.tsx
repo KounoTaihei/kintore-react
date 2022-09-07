12d4452c0f4dbc919673b5a82903ddda8ac24d87
河野太平 <>
2022-09-07 15:56:48 +0900

    タイマーの細かい機能の実装

--- src/App.tsx
+++ src/App.tsx
@@ -7,8 +7,8 @@ import useInterval from './hooks/interval';
 import { Button } from '@mui/material';
 
 const exampleTimer: Timer = {
-  training: 20,
-  rest: 10,
+  training: 5,
+  rest: 3,
   repeat: 3
 }
 
@@ -16,17 +16,42 @@ function App() {
   const [ currentTimer, setCurrentTimer ] = useState<Timer | null>(exampleTimer);
 
   const [ elapsedTime, setElapsedTime ] = useState<number>(0);
+  const [ trainingState, setTrainingState ] = useState<'training' | 'resting'>('training');
+  const [ repeated, setRepeated ] = useState<number>(0);
 
   const interval = useInterval(() => {
-    setElapsedTime(elapsedTime + 0.25);
-  }, 250, false);
+    if(trainingState === 'training') {
+      setElapsedTime(elapsedTime + 0.25);
+      if(elapsedTime === currentTimer?.training!) {
+        if(repeated === currentTimer?.repeat! - 1) {
+          console.log('all done');
+          interval[1].stop();
+        } else {
+          console.log('tarining done');
+          setElapsedTime(0);
+          setTrainingState('resting');
+        }
+      }
+    } else if(trainingState === 'resting') {
+      setElapsedTime(elapsedTime + 0.25);
+      if(elapsedTime === currentTimer?.rest!) {
+        console.log('resting done');
+        setRepeated(repeated + 1);
+        setElapsedTime(0);
+        setTrainingState('training');
+      }
+    }
+  },
+    250,
+    false
+  );
 
   return (
     <div className="p-8">
       <CircularProgressbar
         maxValue={1}
-        value={elapsedTime / currentTimer?.training!}
-        text="65"
+        value={trainingState === 'training' ? elapsedTime / currentTimer?.training! : elapsedTime / currentTimer?.rest!}
+        text={trainingState === 'training' ? `${currentTimer?.training! - Math.floor(elapsedTime)}` : `${currentTimer?.rest! - Math.floor(elapsedTime)}`}
         strokeWidth={6}
         styles={buildStyles({
           pathColor: "pink",
@@ -35,7 +60,9 @@ function App() {
       />
       <Button onClick={interval[1].start}>start</Button>
       <Button onClick={interval[1].stop}>stop</Button>
-      elapsedTime: {Math.floor(elapsedTime)}
+      <div>training state: {trainingState}</div>
+      <div>elapsedTime: {Math.floor(elapsedTime)}</div>
+      <div>repeated time: {repeated}</div>
     </div>
   );
 }
