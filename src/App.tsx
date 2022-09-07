f6620cdd359ec81bad151c994887fc2327003a1a
河野太平 <>
2022-09-07 15:33:27 +0900

    タイマー実装

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,10 @@
-import { useEffect, useState } from 'react';
+import { useEffect, useRef, useState } from 'react';
 import './App.scss';
 import { Timer } from '../types/types';
 import { buildStyles, CircularProgressbar } from 'react-circular-progressbar';
 import 'react-circular-progressbar/dist/styles.css';
+import useInterval from './hooks/interval';
+import { Button } from '@mui/material';
 
 const exampleTimer: Timer = {
   training: 20,
@@ -13,11 +15,17 @@ const exampleTimer: Timer = {
 function App() {
   const [ currentTimer, setCurrentTimer ] = useState<Timer | null>(exampleTimer);
 
+  const [ elapsedTime, setElapsedTime ] = useState<number>(0);
+
+  const interval = useInterval(() => {
+    setElapsedTime(elapsedTime + 0.25);
+  }, 250, false);
+
   return (
     <div className="p-8">
       <CircularProgressbar
         maxValue={1}
-        value={6 / 20}
+        value={elapsedTime / currentTimer?.training!}
         text="65"
         strokeWidth={6}
         styles={buildStyles({
@@ -25,6 +33,9 @@ function App() {
           textColor: "pink",
         })}
       />
+      <Button onClick={interval[1].start}>start</Button>
+      <Button onClick={interval[1].stop}>stop</Button>
+      elapsedTime: {Math.floor(elapsedTime)}
     </div>
   );
 }
